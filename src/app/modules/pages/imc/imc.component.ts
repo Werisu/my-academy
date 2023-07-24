a06ffea9f83a3aca90cc8e57f28f12884eac959d
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-24 10:01:01 -0300

    style

--- src/app/modules/pages/imc/imc.component.ts
+++ src/app/modules/pages/imc/imc.component.ts
@@ -10,11 +10,37 @@ export class ImcComponent {
   peso = 0;
   altura = 0;
   resultado = '';
+  classificacao = '';
+  corClassificacao = '';
 
   // calcular IMC
-  calcularIMC(){
-    let imc = this.peso / (this.altura * this.altura);
+  calcularIMC() {
+    const imc = this.peso / (this.altura * this.altura);
     this.resultado = "Seu IMC é: " + imc.toFixed(2);
-  }
 
+    if (isNaN(imc)) {
+      this.classificacao = '';
+      this.corClassificacao = '';
+    } else {
+      if (imc < 18.5) {
+        this.classificacao = 'Você está abaixo do peso.';
+        this.corClassificacao = 'text-blue-500';
+      } else if (imc >= 18.5 && imc < 24.9) {
+        this.classificacao = 'Seu peso está normal.';
+        this.corClassificacao = 'text-green-500';
+      } else if (imc >= 25 && imc < 29.9) {
+        this.classificacao = 'Você está acima do peso.';
+        this.corClassificacao = 'text-yellow-500';
+      } else if (imc >= 30 && imc < 34.9) {
+        this.classificacao = 'Você tem Obesidade grau I.';
+        this.corClassificacao = 'text-orange-500';
+      } else if (imc >= 35 && imc < 39.9) {
+        this.classificacao = 'Você tem Obesidade grau II.';
+        this.corClassificacao = 'text-red-500';
+      } else {
+        this.classificacao = 'Você tem Obesidade grau III.';
+        this.corClassificacao = 'text-red-800';
+      }
+    }
+  }
 }
--- src/app/modules/pages/imc/imc.module.ts
+++ src/app/modules/pages/imc/imc.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImcComponent } from './imc.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 
